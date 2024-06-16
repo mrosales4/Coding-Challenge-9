ba03e568ffa3ef3a59bc9ad470dd5a364d0d62d6
Matthew A Rosales <matthewrosales@usf.edu>
2024-06-16 14:15:00 -0400

    Bug Fixed Java Script

--- cc_9.js
+++ cc_9.js
@@ -1,11 +1,11 @@
 // U22978120
 
 var d3Script = document.createElement('script');
-d3Script.src = 'https://d3.js.org/d3.v6.min.js';
+d3Script.src = 'https://d3js.org/d3.v6.min.js';
 document.head.appendChild(d3Script);
 
 d3Script.onload = function() {
-
+    
     var container = document.createElement('div');
     container.id = 'container';
     container.style.position = 'relative';
@@ -13,30 +13,29 @@ d3Script.onload = function() {
     document.body.appendChild(container);
 
     var styles = document.createElement('style');
-    styles.textContent =
-        .datapoint {
+    styles.textContent = `
+    .data-point {
         width: 20px;
         height: 20px;
         background-color: gray;
         border: 1px solid black;
         position: absolute;
         transition: background-color 0.3s;
-    }
+        }
     `;
     document.head.appendChild(styles);
 
-    d3select('#container').selectAll('.data-point')
+    d3.select('#container').selectAll('.data-point')
     .data([{x: 10, y: 10}, {x: 70, y:50}, {x: 130, y:90}])
     .enter().append('div')
     .attr('class', 'data-point')
     .style('left', d => `${d.x}px`)
     .style(`top`, d => `${d.y}px`)
     .on('mouseover', function() {
-    `    d3.select(this).style('background-color', 'firebrick');
+        d3.select(this).style('background-color', 'firebrick');
     })
-.on('mouseout', function() {
-    d3.select(this).style('background-color', 'gray');
+    .on('mouseout', function() {
+        d3.select(this).style('background-color', 'gray');
     });
 };
-
         
