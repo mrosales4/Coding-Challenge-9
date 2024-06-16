34ca10f2e936a00d6e98142e3aa12eec4e1c6d4a
Matthew A Rosales <matthewrosales@usf.edu>
2024-06-16 14:11:49 -0400

    Update Java Script to correct version

--- cc_9.js
+++ cc_9.js
@@ -1,30 +1,42 @@
 // U22978120
 
-document.addEventListener('DOMContentLoaded', initialize);
+var d3Script = document.createElement('script');
+d3Script.src = 'https://d3.js.org/d3.v6.min.js';
+document.head.appendChild(d3Script);
 
-const data = [1, 2, 3, 4, 5];
+d3Script.onload = function() {
 
-//Appends the specified data to the specified container.
-function createPointElements(container, data) {
-    container.selectAll('.data-point')
-        .data(data)
-        .enter()
-        .append('div')
-        .attr('class', 'data-point')
-        .text (d => d)
-        .on('mouseover', handleMouseOver)
-        .on('mouseout', handleMouseOut);
-}
+    var container = document.createElement('div');
+    container.id = 'container';
+    container.style.position = 'relative';
+    container.style.height = '200px';
+    document.body.appendChild(container);
 
-//change CSS colors
-function handleMouseOver(event, d) {
-    d3.select(this).style('background-color', 'firebrick');
-}
-function handleMouseOut(event, d) {
-    d3.select(this).style('background-color', null);
-}
+    var styles = document.createElement('style');
+    styles.textContent =
+        .datapoint {
+        width: 20px;
+        height: 20px;
+        background-color: gray;
+        border: 1px solid black;
+        position: absolute;
+        transition: background-color 0.3s;
+    }
+    `;
+    document.head.appendChild(styles);
 
-//Runs CreatePointElements for the selected container and datapoints array.
-function initialize() {
-    createPointElements(d3.select('#dataContainer'), data);
-}
+    d3select('#container').selectAll('.data-point')
+    .data([{x: 10, y: 10}, {x: 70, y:50}, {x: 130, y:90}])
+    .enter().append('div')
+    .attr('class', 'data-point')
+    .style('left', d => `${d.x}px`)
+    .style(`top`, d => `${d.y}px`)
+    .on('mouseover', function() {
+    `    d3.select(this).style('background-color', 'firebrick');
+    })
+.on('mouseout', function() {
+    d3.select(this).style('background-color', 'gray');
+    });
+};
+
+        
